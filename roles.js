43e56f0f6dbe9b93906043bd5c47bcbde4ac5790
iLikeToCode <archie@archiesbytes.xyz>
2026-01-02 13:52:48 +0000

    replace st with noble

--- roles.js
+++ roles.js
@@ -4,7 +4,7 @@ export const roleMap = {
 	// if user has any of the roles in this array, they are given the positive metadata
 	// they can then clame the role in the linked roles section
 	// discord enforces a max of 5 entries here
-	"staff_team":    ["1404896442296041543"],
+	"noble":         ["1453008903179862038"],
 	"high_rank":     ["1418037523623706724"],
 	"directive":     ["1452703259541110784"],
 	"foundership":   ["1404880068248998012"],
@@ -13,10 +13,10 @@ export const roleMap = {
 
 export const rolesToAddViewPerm = {
 	// the key here means nothing, just helps the developer, the value should be the role you added the link requirement to
-	"steam": "1455710499357327430",
-	"hr": "1455710496589090878",
-	"di": "1455710493942611969",
-	"f":  "1455710472019116073",
+	"noble": "1455710499357327430",
+	"hr":    "1455710496589090878",
+	"di":    "1455710493942611969",
+	"f":     "1455710472019116073",
 	"stexa": "1455710346600906833"
 }
 
